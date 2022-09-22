605f2c0a8ce7af2fb41a5d28500f7f87385128fd
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-09-22 08:10:45 -0400

    accomodation for organization changes
    i also have this to make testing easier, instead of typing `tests/copper/test.cop` a billion times

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ print("Enter filepath:")
 filepath = input(">>> ")
 
 try:
-	file = open("tests/" + filepath, 'r')
+	file = open("tests/copper/" + filepath, 'r')
 except FileNotFoundError:
 	print(f"\n\033[0;31mUnknownFileError: Could not find the file '{filepath}'\033[0;0m\n")
 	sys.exit()
