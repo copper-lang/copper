b2d7750a4d799871221e858552917c6f8fe4fe52
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-09-27 15:28:44 +0000

    list declaration

---
+++ src/utils/datatypes/list.py
@@ -0,0 +1,6 @@
+class List:
+	def __init__(self, literal, line, lineno, location):
+		self.literal = literal
+		self.line = line
+		self.lineno = lineno
+		self.location = location
--- src/utils/objects.py
+++ src/utils/objects.py
@@ -2,6 +2,7 @@ from utils.datatypes.string import String
 from utils.datatypes.integer import Integer
 from utils.datatypes.float import Float
 from utils.datatypes.boolean import Boolean
+from utils.datatypes.list import List
 from utils.errors import Error
 import utils.interpreter
 
@@ -26,39 +27,80 @@ class Object:
 			return "string", string
 
 		elif (self.object[0] != "\"" and self.object[-1] == "\"") or (self.object[0] == "\"" and self.object[-1] != "\""):
-			syntaxerror = Error(
-				"SyntaxError",
-				"Missing quotation marks",
-				self.line,
-				self.lineno,
-				self.location
-			)
-			syntaxerror.print_stacktrace()
+			if (self.object[0] == "[" and self.object[-1] != "]") or (self.object[0] != "[" and self.object[-1] == "]"):
+				syntaxerror = Error(
+					"SyntaxError",
+					"Missing brackets",
+					self.line,
+					self.lineno,
+					self.location
+				)
+				syntaxerror.print_stacktrace()
+			
+			else:
+				syntaxerror = Error(
+					"SyntaxError",
+					"Missing quotation marks",
+					self.line,
+					self.lineno,
+					self.location
+				)
+				syntaxerror.print_stacktrace()
 
 		elif self.vars.get("".join(self.object)):
 			return "variable", self.vars.get("".join(self.object))
 
 		else:
-			try:
-				self.object = "".join(self.object)
-				if "^" in self.object:
-					self.object.replace("^", "**")
+			if self.object[0] == "[" and self.object[-1] == "]":
+				self.object = list(self.object)
+				self.object.pop(0)
+				self.object.pop()
 
-				keys = list(self.vars.keys())
-				variables = {}
-				for i in range(len(self.vars)):
-					variables[keys[i]] = self.vars.get(keys[i]).literal
+				elements = "".join(self.object).split(",")
+				for i in range(len(elements)):
+					elements[i] = elements[i].strip()
 
-				result = eval(self.object, variables)
+				try:
+					for i in range(len(elements)):
+						if elements[i][0] == "\"" and elements[i][-1] != "\"":
+							string = ""
+							index = i
+							while True:
+								if elements[index][-1] == "\"":
+									string += elements[index]
+									break_index = index
+									break
+								else:
+									string += elements[index] + ","
+									index += 1
+	
+							for set in elements[i:break_index + 1]:
+								elements.remove(set)
+	
+							elements.insert(i, string)
+				
+				except IndexError:
+					pass
 
-				if isinstance(result, int):
-					integer = Integer(result, self.line, self.lineno, self.location)
-					return "math", integer
-				else:
-					decimal = Float(result, self.line, self.lineno, self.location)
-					return "math", decimal
+				for i in range(len(elements)):
+					object = Object(elements[i], self.vars, self.functions, self.isFunction, self.function, self.line, self.lineno, self.location)
+					type = object.checkType()
+					elements[i] = type[1]
 
-			except (SyntaxError, NameError, AttributeError):
+				lst = List(elements, self.line, self.lineno, self.location)
+				return "list", lst
+
+			elif (self.object[0] == "[" and self.object[-1] != "]") or (self.object[0] != "[" and self.object[-1] == "]"):
+				syntaxerror = Error(
+					"SyntaxError",
+					"Missing brackets",
+					self.line,
+					self.lineno,
+					self.location
+				)
+				syntaxerror.print_stacktrace()
+
+			else:
 				try:
 					integer = Integer(int("".join(self.object)), self.line, self.lineno, self.location)
 					return "integer", integer
@@ -67,52 +109,72 @@ class Object:
 						decimal = Float(float("".join(self.object)), self.line, self.lineno, self.location)
 						return "float", decimal
 					except ValueError:
-						if "".join(self.object) == "True" or "".join(self.object) == "False":
-							boolean = Boolean("".join(self.object) == "True", self.line, self.lineno, self.location)
-							return "boolean", boolean
-
-						else:
-							try:
-								command = ""
-								i = 0
-								while True:
-									if self.object[i] == "(":
-										break
-									else:
-										command += self.object[i]
-										i += 1
-
-							except IndexError:
-								literal = "".join(self.object)
-								ile = Error(
-									"InvalidLiteralError",
-									f"Invalid literal '{literal}'",
-									self.line,
-									self.lineno,
-									self.location
-								)
-								ile.print_stacktrace()
-
+						try:
+							self.object = "".join(self.object)
+							if "^" in self.object:
+								self.object.replace("^", "**")
+			
+							keys = list(self.vars.keys())
+							variables = {}
+							for i in range(len(self.vars)):
+								variables[keys[i]] = self.vars.get(keys[i]).literal
+			
+							result = eval(self.object, variables)
+			
+							if isinstance(result, int):
+								integer = Integer(result, self.line, self.lineno, self.location)
+								return "math", integer
 							else:
-								interpreter = utils.interpreter.Interpreter(
-									self.object,
-									self.vars,
-									self.functions,
-									self.isFunction,
-									self.function,
-									self.lineno,
-									self.location
-								)
-
-								inpt = String(interpreter.interpret(), self.line, self.lineno, self.location)
-								return "input", inpt
-
-			except TypeError:
-				matherror = Error(
-					"MathError",
-					f"Could not evaluate the expression '{self.object}'",
-					self.line,
-					self.lineno,
-					self.location
-				)
-				matherror.print_stacktrace()
+								decimal = Float(result, self.line, self.lineno, self.location)
+								return "math", decimal
+			
+						except (SyntaxError, NameError, AttributeError):
+							if "".join(self.object) == "True" or "".join(self.object) == "False":
+								boolean = Boolean("".join(self.object) == "True", self.line, self.lineno, self.location)
+								return "boolean", boolean
+		
+							else:
+								try:
+									command = ""
+									i = 0
+									while True:
+										if self.object[i] == "(":
+											break
+										else:
+											command += self.object[i]
+											i += 1
+		
+								except IndexError:
+									literal = "".join(self.object)
+									ile = Error(
+										"InvalidLiteralError",
+										f"Invalid literal '{literal}'",
+										self.line,
+										self.lineno,
+										self.location
+									)
+									ile.print_stacktrace()
+		
+								else:
+									interpreter = utils.interpreter.Interpreter(
+										self.object,
+										self.vars,
+										self.functions,
+										self.isFunction,
+										self.function,
+										self.lineno,
+										self.location
+									)
+		
+									inpt = String(interpreter.interpret(), self.line, self.lineno, self.location)
+									return "input", inpt
+			
+						except TypeError:
+							matherror = Error(
+								"MathError",
+								f"Could not evaluate the expression '{self.object}'",
+								self.line,
+								self.lineno,
+								self.location
+							)
+							matherror.print_stacktrace()
